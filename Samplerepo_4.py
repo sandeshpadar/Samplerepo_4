1688c22c3f9bb4f5750331580f11e700fc26d387
sandeshpadar <sandeshpadar1@gmail.com>
2023-10-26 07:54:40 +0530

    For my fourth commit

---
+++ Samplerepo_4.py
@@ -0,0 +1,7 @@
+# how to find LCM
+
+import math
+num1 = int(input('Enter the first number:- '))
+num2 = int(input('Enter the second number:- '))
+LCM = math.lcm(num1, num2)
+print(f'The LCM of {num1} and {num2} is {LCM}')
